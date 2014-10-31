f8085effefc6ddadbcba0901e03ee59bde5d795c
MikeProteus97 <mikeandroid97@gmail.com>
2014-10-31 15:26:22 -0500

    Arduino+Java
    Prender y encender un LED

---
+++ src/arduinonano/ArduinoNano.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arduinonano;
+
+/**
+ *
+ * @author Familia Vargas
+ */
+public class ArduinoNano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Window ventana=new Window();
+    ventana.setVisible(true);
+    ventana.setDefaultCloseOperation(ventana.EXIT_ON_CLOSE);
+        ventana.setLocationRelativeTo(null);
+// TODO code application logic here
+    }
+    
+}
